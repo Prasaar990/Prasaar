eed053a087db3f54081d4c0f1bfea8a7b798da90
Prasaar990 <ashok@prasaar.co>
2025-02-16 11:47:51 +0530

    added animation to features

--- src/components/main/Feature.jsx
+++ src/components/main/Feature.jsx
@@ -1,20 +1,65 @@
+import { motion } from "framer-motion";
 import styles from "../../styles/Feature.module.css";
 
 /* eslint-disable */
 export default function Feature({ title, list, side = "" }) {
   return (
-    <section className={`${styles[side]} ${styles.feature}`}>
-      <div className={styles.imgDiv}>
-        <img src="./img/1.png" alt="feature 1" className={styles.img} />
-      </div>
-      <div className={styles.text}>
-        <h1>{title}</h1>
-        <ul>
-          {list.map((item) => {
-            return <li key={item}>{item}</li>;
-          })}
-        </ul>
-      </div>
-    </section>
+    <>
+      {side !== "rowReverse" ? (
+        <section className={`${styles.feature}`}>
+          <motion.div
+            initial={{ x: "-30%", opacity: 0.1 }}
+            whileInView={{ x: "0%", opacity: 1 }}
+            transition={{ duration: 0.8, ease: "easeOut" }}
+            viewport={{ once: true }}
+            className={styles.imgDiv}
+          >
+            <img src="./img/1.png" alt="feature 1" className={styles.img} />
+          </motion.div>
+          <motion.div
+            initial={{ x: "30%", opacity: 0.1 }}
+            whileInView={{ x: "0%", opacity: 1 }}
+            transition={{ duration: 0.8, ease: "easeOut" }}
+            viewport={{ once: true }}
+            className={styles.text}
+          >
+            <h1>{title}</h1>
+            <ul>
+              {list.map((item) => {
+                return <li key={item}>{item}</li>;
+              })}
+            </ul>
+          </motion.div>
+        </section>
+      ) : (
+        <>
+          <section className={`${styles.feature}`}>
+            <motion.div
+              initial={{ x: "-30%", opacity: 0.1 }}
+              whileInView={{ x: "0%", opacity: 1 }}
+              viewport={{ once: true }}
+              transition={{ duration: 0.8, ease: "easeOut" }}
+              className={styles.text}
+            >
+              <h1>{title}</h1>
+              <ul>
+                {list.map((item) => {
+                  return <li key={item}>{item}</li>;
+                })}
+              </ul>
+            </motion.div>
+            <motion.div
+              initial={{ x: "30%", opacity: 0.1 }}
+              viewport={{ once: true }}
+              whileInView={{ x: "0%", opacity: 1 }}
+              transition={{ duration: 0.8, ease: "easeOut" }}
+              className={styles.imgDiv}
+            >
+              <img src="./img/1.png" alt="feature 1" className={styles.img} />
+            </motion.div>
+          </section>
+        </>
+      )}
+    </>
   );
 }
--- src/components/main/Features.jsx
+++ src/components/main/Features.jsx
@@ -6,16 +6,30 @@ export default function Features() {
     <div className={styles.features}>
       <Feature
         title={"Unified Platform"}
-        list={["first", "second", "third", "fourth"]}
+        list={[
+          "An integrated center for all your customer needs.",
+          "Our Unified Platform consolidates all your customer channels, feedback, and sales into one user-friendly interface.",
+          "This seamless access allows for swift interactions and effective management, contributing to better customer engagement and retention.",
+          "Keep track, communicate and engage — all from one place.",
+        ]}
       />
       <Feature
-        title={"Unified Platform"}
-        list={["first", "second", "third", "fourth"]}
+        title={"Automation - powered by AI"}
+        list={[
+          "Improve your workflow with our AI-powered automation.",
+          "From providing quick, personalized responses to identifying and addressing negative feedback, our AI automation makes your customer service easier and more efficient.",
+          "Say goodbye to manual oversight and hello to automatic, immediate, and thoughtful interactions for all your customers",
+        ]}
         side={"rowReverse"}
       />
       <Feature
-        title={"Unified Platform"}
-        list={["first", "second", "third", "fourth"]}
+        title={"Customer Analytics & Insights"}
+        list={[
+          "Stay informed and always a step ahead.",
+          "Get an in-depth understanding of your customer base at every location with actionable insights, their behavior, and location leaderboards.",
+          "Make data-driven decisions, understand what works and what doesn't, and measure success with our comprehensive analytics.",
+          "Utilize these insights to refine and retarget your business strategies, bringing back customers and success for your business.",
+        ]}
       />
     </div>
   );
