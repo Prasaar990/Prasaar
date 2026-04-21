910e2688bec482afac7c5a359d97e375c869fe3c
Adinath <adinathsyadav2016@gmail.com>
2026-04-21 10:57:38 +0530

    added powered by and mobile number

--- src/components/main/PublicCanvasPage.jsx
+++ src/components/main/PublicCanvasPage.jsx
@@ -507,6 +507,21 @@ const PublicCanvasPage = () => {
               </>
             )}
           </div>
+
+          <div className='flex items-center justify-center gap-2 pb-4'>
+            Powered by Prasaar •{' '}
+            <a
+              href="tel:+919960969076"
+              className="inline-flex items-center gap-1 font-semibold"
+              
+            >
+              <svg className="w-3.5 h-3.5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
+                  d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.948V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
+              </svg>
+              +91 9960969076
+            </a>
+          </div>
         </div>
 
         {/* Footer tip */}
